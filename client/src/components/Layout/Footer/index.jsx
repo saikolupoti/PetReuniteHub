fe2ac3cc816dc73ee5fc91788ca9490672f6c28d
saikolupoti <141009853+saikolupoti@users.noreply.github.com>
2024-09-06 20:34:25 +0530

    init

---
+++ client/src/components/Layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className="general_footer">
+      <span className="copyright">Ethnus Team - 130, {new Date().getFullYear()}</span>
+        
+    </footer>
+  )
+}
+
+export default Footer;
